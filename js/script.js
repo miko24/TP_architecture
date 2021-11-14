68e727e1fa33b9200752ae01894931f7aafcc546
miko <miko@kali.griem.com>
2021-11-14 22:00:06 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+
+$("#add_btn").on("click",()=>{
+    if($("#todo").val() == ""){
+        return alert("il est obligatoire de remplir la zone de texte")
+    }
+    addTodo()
+    $("#todo").val("")
+})
+document.addEventListener("keyup",(event)=>{
+    if(event.keyCode === 13 && $("#todo").val() !== ""){
+        addTodo()
+        return $("#todo").val("")
+    }
+    else if(event.keyCode === 13 && $("#todo").val() === ""){
+        alert("il est obligatoire de remplir la zone de texte")
+    }
+    
+    
+})
+
+$(document).on("click",".formation",(event)=>{
+    if (event.target.className == 'edit'){
+        return alert('s')    
+        }
+        $(event.target).parent().next().toggleClass("active")
+    
+
+})
+
+
+$(document).on("click","#btn",(event)=>{
+    var organisme= $(".organisme")
+    var type= $(".type")
+    var date= $(".date")
+    // console.log($(event.target).parent().find(".detail-organisme"))
+    $(event.target).parent().find(".detail-organisme").append(organisme.val())
+   $(event.target).parent().find(".detail-type").append(type.val())
+    $(event.target).parent().find(".detail-date").append(date.val())
+    organisme.hide()
+    type.hide()
+    date.hide()
+    $(event.target).hide()
+
+})
+
+function addTodo(){
+        var textval = $("#todo").val()
+        $("#todos-list").append(`<li class='formation'>
+        <div class="toggle-on"></div>
+        <p> ${textval} </p>
+        <button class="edit" ></button>
+        <button class="delete" ></button>
+        </li>
+        <div class="details active" >
+                <div>
+                    <p class="detail-organisme"> Organisme responsable :  </p> <input class="organisme" type="text" placeholder="modifier l'organisme">
+                </div>
+                <br>
+                <div>
+                    <p class="detail-type"> type:  </p> <input class="type" type="text" placeholder="modifier type diplôme">
+                </div>
+                <br>
+                <div>
+                    <p class="detail-date"> Date de la fornation :  </p> <input class="date" type="text" placeholder="modifier la date">
+                </div>
+                <br>
+                <div>
+                    <p>Terminée ou en cours</p>  <input class="check" type="checkbox" checked="false">
+                </div>
+                <br> <br>
+                <input id="btn" type="button" value="Valider les modifications">
+            </div>`)
+
+              
+}
